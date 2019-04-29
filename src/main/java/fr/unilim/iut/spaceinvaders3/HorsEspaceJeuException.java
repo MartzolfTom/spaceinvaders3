447b1b6105485f616d46afd9b41b407e7cf7a6d7
tom <tom.martzolf@etu.unilim.fr>
2019-04-29 08:59:41 +0200

    fonctionalité 1 : story 2 : positionner un nouveau vaisseau dans l'esapce de jeu

---
+++ src/main/java/fr/unilim/iut/spaceinvaders3/HorsEspaceJeuException.java
@@ -0,0 +1,9 @@
+package fr.unilim.iut.spaceinvaders3;
+
+@SuppressWarnings("serial")
+class HorsEspaceJeuException extends RuntimeException {
+
+    public HorsEspaceJeuException(String message) {
+	    super(message);
+    }
+}
